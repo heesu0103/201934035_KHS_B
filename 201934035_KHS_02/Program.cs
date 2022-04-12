640c13e82bf9b0410fe601b59a473439dcd6439c
rhtmaehcl0103@naver.com <rhtmaehcl0103@naver.com>
2022-04-13 06:45:34 +0900

    초기 커밋.

---
+++ 201934035_KHS_02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _201934035_KHS_02
+{
+    class MainApp2
+    {
+        public static void Swap(int a, int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+
+            Console.WriteLine("x:{0}, y:{1}", x, y);
+
+            Swap(x, y);
+
+            Console.WriteLine("x:{0}, y:{1}", x, y);
+        }
+    }
+}
---
+++ 201934035_KHS_04/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _201934035_KHS_04
+{
+    class MainApp4
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            for (; ; )
+                Console.WriteLine(i++);
+        }
+    }
+}
